54aab2364c99b5f2e6276b21f718bbc65f5fbdb7
coty <Coty.Janeway@gmail.com>
2022-09-21 04:55:04 -0700

    Modify modal so I can remove View button.

--- client/src/components/CharacterCard.jsx
+++ client/src/components/CharacterCard.jsx
@@ -4,17 +4,29 @@ import CharacterModal from './CharacterModal.jsx';
 class CharacterCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      openModal: false
+    }
+  }
+
+  onClickButton = event => {
+    event.preventDefault();
+    this.setState({ openModal: true })
+  }
+
+  onCloseModal = () => {
+    this.setState({ openModal: false });
   }
 
   render() {
     return (
       <div className="character-card">
-        <div>
+        <div onClick={this.onClickButton}>
           <h2 className="character-name-on-card">
             {this.props.singleChar.name}
           </h2>
 
-          <div>
+          <div className="character-description">
             <div>{`Lvl. ${this.props.singleChar.level}`}</div>
             <div>{`Ancestry: ${this.props.singleChar.ancestry}`}</div>
             <div>{`Class:  ${this.props.singleChar.class}`}</div>
@@ -27,7 +39,85 @@ class CharacterCard extends React.Component {
 
           </div>
         </div>
-        <footer><CharacterModal character={this.props.singleChar} /></footer>
+
+        {/* <footer><CharacterModal character={this.props.singleChar} /></footer> */}
+        {this.state.openModal
+          ?
+          <div
+            open={this.state.openModal}
+            onClose={this.onCloseModal}
+          >
+            <div className="character-modal-background">
+              <div className="character-modal">
+
+                <h1 className="character-description-modal">
+                  {`${this.props.singleChar.name}, Level ${this.props.singleChar.level} ${this.props.singleChar.ancestry} ${this.props.singleChar.class} (${this.props.singleChar.background})`}
+                </h1>
+
+                <div className="modal-grid-container">
+
+                  {/* Perception and Class DC Grid */}
+                  <div className="grid-list">
+                    <h2 className="attribute-text">Perception:</h2>
+                    <h2 className="attribute-number-1">{this.props.singleChar.perception}</h2>
+                    <h2 className="attribute-text">Class DC:</h2>
+                    <h2 className="attribute-number-1">{this.props.singleChar.classdc}</h2>
+                  </div>
+
+                  {/* Saving Throws Grid */}
+                  <div className="grid-list">
+                    <div className="attribute-text"> Fortitude: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.fortitude} </div>
+                    <div className="attribute-text"> Reflex: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.reflex} </div>
+                    <div className="attribute-text"> Will: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.will} </div>
+                  </div>
+
+                  {/* Abilities Grid */}
+                  <div className="grid-list">
+                    <div className="attribute-text"> Strength: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.strength} </div>
+                    <div className="attribute-text"> Dexterity: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.dexterity} </div>
+                    <div className="attribute-text"> Constitution: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.constitution} </div>
+                    <div className="attribute-text"> Intelligence: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.intelligence} </div>
+                    <div className="attribute-text"> Wisdom: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.wisdom} </div>
+                    <div className="attribute-text"> Charisma: </div>
+                    <div className="attribute-number-2"> {this.props.singleChar.charisma} </div>
+                  </div>
+
+                  {/* Skills Grid */}
+                  <div className="grid-list">
+                    <div className="attribute-text">Arcana:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.arcana}</div>
+                    <div className="attribute-text">Crafting:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.crafting}</div>
+                    <div className="attribute-text">Nature:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.nature}</div>
+                    <div className="attribute-text">Religion:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.religion}</div>
+                    <div className="attribute-text">Occultism:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.occultism}</div>
+                    <div className="attribute-text">Society:</div>
+                    <div className="attribute-number-2">{this.props.singleChar.society}</div>
+                  </div>
+                </div>
+
+                <button
+                  onClick={this.onCloseModal}
+                  // onClose={() => this.onCloseModal}
+                  className="char-preview-button"
+                > Close</button>
+              </div>
+            </div>
+
+          </div>
+          : ''
+        }
       </div>
     )
   }
--- client/src/components/CharacterModal.jsx
+++ client/src/components/CharacterModal.jsx
@@ -1,111 +1,114 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class CharacterModal extends Component {
+// class CharacterModal extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      openModal: false
-    }
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       openModal: false
+//     }
+//   }
 
-  onClickButton = event => {
-    event.preventDefault()
-    this.setState({ openModal: true })
-  }
+//   onClickButton = (event) => {
+//     event.preventDefault();
+//     console.log("clicked2");
+//     this.setState({ openModal: true });
+//   }
 
-  onCloseModal = () => {
-    this.setState({ openModal: false })
-  }
+//   onCloseModal = () => {
+//     console.log("clicked2");
 
-  render() {
-    return (
-      <div>
+//     this.setState({ openModal: false });
+//   }
 
-        <button onClick={this.onClickButton} className="character-view-button">
-          View
-        </button>
+//   render() {
+//     return (
+//       <div onClick={this.onClickButton}>
 
-        {this.state.openModal
-          ?
-          <div
-            open={this.state.openModal}
-            onClose={this.onCloseModal}
-          >
-            <div className="character-modal-background">
-              <div className="character-modal">
+//         <button className="character-view-button">
+//           View
+//         </button>
 
-                <h1 className="character-description-modal">
-                  {`${this.props.character.name}, Level ${this.props.character.level} ${this.props.character.ancestry} ${this.props.character.class} (${this.props.character.background})`}
-                </h1>
+//         {this.state.openModal
+//           ?
+//           <div
+//             open={this.state.openModal}
+//             onClose={this.onCloseModal}
+//           >
+//             <div className="character-modal-background">
+//               <div className="character-modal">
 
-                <div className="modal-grid-container">
+//                 <h1 className="character-description-modal">
+//                   {`${this.props.character.name}, Level ${this.props.character.level} ${this.props.character.ancestry} ${this.props.character.class} (${this.props.character.background})`}
+//                 </h1>
 
-                  {/* Perception and Class DC Grid */}
-                  <div className="grid-list">
-                    <h2 className="attribute-text">Perception:</h2>
-                    <h2 className="attribute-number-1">{this.props.character.perception}</h2>
-                    <h2 className="attribute-text">Class DC:</h2>
-                    <h2 className="attribute-number-1">{this.props.character.classdc}</h2>
-                  </div>
+//                 <div className="modal-grid-container">
 
-                  {/* Saving Throws Grid */}
-                  <div className="grid-list">
-                    <div className="attribute-text"> Fortitude: </div>
-                    <div className="attribute-number-2"> {this.props.character.fortitude} </div>
-                    <div className="attribute-text"> Reflex: </div>
-                    <div className="attribute-number-2"> {this.props.character.reflex} </div>
-                    <div className="attribute-text"> Will: </div>
-                    <div className="attribute-number-2"> {this.props.character.will} </div>
-                  </div>
+//                   {/* Perception and Class DC Grid */}
+//                   <div className="grid-list">
+//                     <h2 className="attribute-text">Perception:</h2>
+//                     <h2 className="attribute-number-1">{this.props.character.perception}</h2>
+//                     <h2 className="attribute-text">Class DC:</h2>
+//                     <h2 className="attribute-number-1">{this.props.character.classdc}</h2>
+//                   </div>
 
-                  {/* Abilities Grid */}
-                  <div className="grid-list">
-                    <div className="attribute-text"> Strength: </div>
-                    <div className="attribute-number-2"> {this.props.character.strength} </div>
-                    <div className="attribute-text"> Dexterity: </div>
-                    <div className="attribute-number-2"> {this.props.character.dexterity} </div>
-                    <div className="attribute-text"> Constitution: </div>
-                    <div className="attribute-number-2"> {this.props.character.constitution} </div>
-                    <div className="attribute-text"> Intelligence: </div>
-                    <div className="attribute-number-2"> {this.props.character.intelligence} </div>
-                    <div className="attribute-text"> Wisdom: </div>
-                    <div className="attribute-number-2"> {this.props.character.wisdom} </div>
-                    <div className="attribute-text"> Charisma: </div>
-                    <div className="attribute-number-2"> {this.props.character.charisma} </div>
-                  </div>
+//                   {/* Saving Throws Grid */}
+//                   <div className="grid-list">
+//                     <div className="attribute-text"> Fortitude: </div>
+//                     <div className="attribute-number-2"> {this.props.character.fortitude} </div>
+//                     <div className="attribute-text"> Reflex: </div>
+//                     <div className="attribute-number-2"> {this.props.character.reflex} </div>
+//                     <div className="attribute-text"> Will: </div>
+//                     <div className="attribute-number-2"> {this.props.character.will} </div>
+//                   </div>
 
-                  {/* Skills Grid */}
-                  <div className="grid-list">
-                    <div className="attribute-text">Arcana:</div>
-                    <div className="attribute-number-2">{this.props.character.arcana}</div>
-                    <div className="attribute-text">Crafting:</div>
-                    <div className="attribute-number-2">{this.props.character.crafting}</div>
-                    <div className="attribute-text">Nature:</div>
-                    <div className="attribute-number-2">{this.props.character.nature}</div>
-                    <div className="attribute-text">Religion:</div>
-                    <div className="attribute-number-2">{this.props.character.religion}</div>
-                    <div className="attribute-text">Occultism:</div>
-                    <div className="attribute-number-2">{this.props.character.occultism}</div>
-                    <div className="attribute-text">Society:</div>
-                    <div className="attribute-number-2">{this.props.character.society}</div>
-                  </div>
-                </div>
+//                   {/* Abilities Grid */}
+//                   <div className="grid-list">
+//                     <div className="attribute-text"> Strength: </div>
+//                     <div className="attribute-number-2"> {this.props.character.strength} </div>
+//                     <div className="attribute-text"> Dexterity: </div>
+//                     <div className="attribute-number-2"> {this.props.character.dexterity} </div>
+//                     <div className="attribute-text"> Constitution: </div>
+//                     <div className="attribute-number-2"> {this.props.character.constitution} </div>
+//                     <div className="attribute-text"> Intelligence: </div>
+//                     <div className="attribute-number-2"> {this.props.character.intelligence} </div>
+//                     <div className="attribute-text"> Wisdom: </div>
+//                     <div className="attribute-number-2"> {this.props.character.wisdom} </div>
+//                     <div className="attribute-text"> Charisma: </div>
+//                     <div className="attribute-number-2"> {this.props.character.charisma} </div>
+//                   </div>
 
-                <button
-                  onClick={this.onCloseModal}
-                  onClose={() => this.onCloseModal}
-                  className="char-preview-button"
-                > Close</button>
-              </div>
-            </div>
+//                   {/* Skills Grid */}
+//                   <div className="grid-list">
+//                     <div className="attribute-text">Arcana:</div>
+//                     <div className="attribute-number-2">{this.props.character.arcana}</div>
+//                     <div className="attribute-text">Crafting:</div>
+//                     <div className="attribute-number-2">{this.props.character.crafting}</div>
+//                     <div className="attribute-text">Nature:</div>
+//                     <div className="attribute-number-2">{this.props.character.nature}</div>
+//                     <div className="attribute-text">Religion:</div>
+//                     <div className="attribute-number-2">{this.props.character.religion}</div>
+//                     <div className="attribute-text">Occultism:</div>
+//                     <div className="attribute-number-2">{this.props.character.occultism}</div>
+//                     <div className="attribute-text">Society:</div>
+//                     <div className="attribute-number-2">{this.props.character.society}</div>
+//                   </div>
+//                 </div>
 
-          </div>
-          : ''
-        }
-      </div>
-    )
-  }
-}
+//                 <button
+//                   onClick={this.onCloseModal}
+//                   onClose={() => this.onCloseModal}
+//                   className="char-preview-button"
+//                 > Close</button>
+//               </div>
+//             </div>
 
-export default CharacterModal;
+//           </div>
+//           : ''
+//         }
+//       </div>
+//     )
+//   }
+// }
+
+// export default CharacterModal;
